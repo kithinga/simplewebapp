a469048cff74e8a4b07779174bec62c13b359b85
Festo <menscfesz@gmail.com>
2022-01-12 22:12:52 +0300

    kithinga

--- vanilla.js
+++ vanilla.js
@@ -26,23 +26,9 @@
     $(".modes").css({display:"none"})
   })
   
-
-
   $(".modes-content").mouseleave(function(){
     $(".modes").css({display:"none"})
   })
-
-  
-
-//   const setBg = () => {
-//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-//     document.body.style.backgroundColor = "#" + randomColor;
-//     color.innerHTML = "#" + randomColor;
-//   }
-  
-//   genNew.addEventListener("click", setBg);
-// setBg();
-
 
 
 $(".addcl").click(function () {
