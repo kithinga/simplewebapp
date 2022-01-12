a48ca266ff65fb026dbed3c1a3eed32463a849ef
Festo <menscfesz@gmail.com>
2022-01-12 23:00:34 +0300

    kithinga commiting

--- navbar.php
+++ navbar.php
@@ -14,25 +14,59 @@
 
 <body>
 
-    <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark p-4">
-            <h5 class="text-white h4">Collapsed content</h5>
-            <span class="text-muted">Toggleable via the navbar brand.</span>
-        </div>
-    </div>
-    <nav class="navbar navbar-dark bg-dark">
+
+    <!--collapsible navbar -->
+    <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="material-icons bars">
+                        list
+                    </span>
+                </button>
+            </div>
+
+            <div class="navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <button class="btni" onclick="location.href=' ideas.php'">
+                        <li>ideas</li>
+                    </button>
+                    <button class="btni" onclick="location.href='sketches.php'">
+                        <li>Sketches</li>
+                    </button>
+                    <button class="btni" onclick="location.href='todos.php'">
+                        <li>Tasks</li>
+                    </button>
+                    <button class="btni" onclick="location.href='colours.php'">
+                        <li>Colours</li>
+                    </button>
+                    <button class="btni" onclick="location.href='artkit.php'">
+                        <li>Artkit</li>
+                    </button>
+                    <button class="btni" onclick="location.href='websites.php'">
+                        <li>Websites</li>
+                    </button>
+                    <button class="btni" onclick="location.href='design.php'">
+                        <li>Designs</li>
+                    </button>
+                </ul>
+            </div>
         </div>
     </nav>
 
+
+
+
+
+
+
+    <!-- Web navbar  -->
+
+
     <div class="navb">
         <div class="items">
-            <button class="btni" onclick="location.href='ideas.php'">
+            <button class="btni" onclick="location.href=' ideas.php'">
                 <li>ideas</li>
             </button>
             <button class="btni" onclick="location.href='sketches.php'">
@@ -55,6 +89,9 @@
             </button>
         </div>
     </div>
+
+
+
     <script src="vanilla.js"></script>
     <script>
     AOS.init();
