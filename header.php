c611046560a2fb317ebf97f7bbb23f0c05140089
Festo <menscfesz@gmail.com>
2022-01-12 14:45:40 +0300

    Kithinga

--- header.php
+++ header.php
@@ -6,28 +6,29 @@ session_start();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Kithinga spa</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kithinga spa</title>
 </head>
 
 <body>
-  <div class="container-fluid ">
-    <div class="head row no-gutters myhead">
-      <div class="col-lg-2">
-           <div class="home"><a href="index.php">
-          <p><span class="inov material-icons md-24">home</span></p>
-        </a></div>
-      </div>
-      <div class="col-lg-8">
-        <div class="name">Manage my mind</div>
-      </div>
-      <div class="col-lg-2">
-        <div class="search"><span class="loki material-icons md-48">perm_identity</span></div>
-      </div>
+    <div class="container-fluid ">
+        <div class="head row no-gutters myhead">
+            <div class="col-lg-2">
+                <div class="home">
+                    <p onclick="location.href='index.php'"><span class="inov material-icons md-24">home</span></p>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="name">Manage my mind</div>
+            </div>
+            <div class="col-lg-2">
+                <div class="search"><span type="button" class="loki material-icons md-48">perm_identity</span></div>
+            </div>
+        </div>
     </div>
-  </div>
-  <script src="vanilla.js"></script>
+    <script src="vanilla.js"></script>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -5,118 +5,120 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vanilla</title>
 </head>
+
 <body>
-<?php
+    <?php
 include 'database.php';
 include 'links.php';
 include 'header.php';
 ?>
-  <div class="container-fluid all-slots">
-    <!-- section one -->
-    <!-- small section with watch and words -->
-    <div class="row no gutters intro">
-     <div class="col-lg-3 decoy1">
-       
-       </div>
-       <div class="col-lg-3 quotes">
-           <h5 class="quote-head">Self Mastery</h5>
-           <p class="quote">working to tap all my potentials!</p>
-       </div>
-       <div class="col-lg-3 times">
-           <h5 class="clock-time">O 1 O O 1 1</h5>
-           <p class="clock-date">01 03 2022</p>
-       </div>
-       <div class="col-lg-3 decoy2">
-       
-       </div>
-    </div>
+    <div class="container-fluid all-slots">
+        <!-- section one -->
+        <!-- small section with watch and words -->
+        <div class="row no gutters intro">
+            <div class="col-lg-3 decoy1">
 
-
-    <div class="row no-gutters">
-      <div class="col-lg-3">
-
-      </div>
-      <div class="col-lg-8">
-        <div class="row no-gutters sec-one" data-aos="fade-down" data-aos-duration="2000">
-          <div onclick="location.href='ideas.php'" class="col-lg-3 ">
-            <div class="idea-items ha">
-              <span class="icon-sec material-icons">batch_prediction</span>
-              <h4>Ideas</h4>
             </div>
-          </div>
-          <div onclick="location.href='sketches.php'" class="col-lg-6 col-xs-6">
-            <div class="sketches-items ha">
-              <span class="icon-sec material-icons">draw</span>
-              <h4>Sketches</h4>
+            <div class="col-lg-3 quotes">
+                <h5 class="quote-head">Self Mastery</h5>
+                <p class="quote">working to tap all my potentials!</p>
             </div>
-          </div>
+            <div class="col-lg-3 times">
+                <h5 class="clock-time">O 1 O O 1 1</h5>
+                <p class="clock-date">01 03 2022</p>
+            </div>
+            <div class="col-lg-3 decoy2">
+
+            </div>
         </div>
-      </div>
-      <div class="col-lg-3"></div>
-    </div>
 
 
+        <div class="row no-gutters">
+            <div class="col-lg-3">
 
-    <!-- section two-->
-    <div class="row no-gutters">
-      <div class="col-lg-3"></div>
-      <div class="col-lg-6">
-        <div class="row sec-two no-gutters">
-          <div onclick="location.href='todos.php'" class="col-lg-4">
-            <div class="task-items ha" data-aos="zoom-out" data-aos-duration="3000">
-              <span class="icon-sec material-icons">task</span>
-              <h4>Tasks</h4>
             </div>
-          </div>
-          <div onclick="location.href='colours.php'" class="col-lg-4">
-            <div class="colour-items ha" data-aos="zoom-in" data-aos-duration="3000">
-              <span class="icon-sec material-icons">palette</span>
-              <h4>Colours</h4>
+            <div class="col-lg-8">
+                <div class="row no-gutters sec-one" data-aos="fade-down" data-aos-duration="2000">
+                    <div onclick="location.href='ideas.php'" class="col-lg-3 ">
+                        <div class="idea-items ha">
+                            <span class="icon-sec material-icons">batch_prediction</span>
+                            <h4>Ideas</h4>
+                        </div>
+                    </div>
+                    <div onclick="location.href='sketches.php'" class="col-lg-6 col-xs-6">
+                        <div class="sketches-items ha">
+                            <span class="icon-sec material-icons">draw</span>
+                            <h4>Sketches</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div onclick="location.href='artkit.php'" class="col-lg-4">
-            <div class="artkit-items ha" data-aos="fade-left" data-aos-duration="3000">
-              <span class="icon-sec material-icons">weekend</span>
-              <h4>Artkit</h4>
-            </div>
-          </div>
+            <div class="col-lg-3"></div>
         </div>
-      </div>
-      <div class="col-lg-3"></div>
-    </div>
 
 
-    <!--section  three-->
-    <div class="row no-gutters">
-      <div class="col-lg-3"></div>
-      <div class="col-lg-6">
-        <div class="row sec-three no-gutters" data-aos="fade-up" data-aos-duration="1500">
-          <div onclick="location.href='websites.php'" class="col-lg-8">
-            <div class="website-items ha">
-              <span class="icon-sec material-icons">language</span>
-              <h4>Websites</h4>
+
+        <!-- section two-->
+        <div class="row no-gutters">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <div class="row sec-two no-gutters">
+                    <div onclick="location.href='todos.php'" class="col-lg-4">
+                        <div class="task-items ha" data-aos="fade-up" data-aos-duration="1500">
+                            <span class="icon-sec material-icons">task</span>
+                            <h4>Tasks</h4>
+                        </div>
+                    </div>
+                    <div onclick="location.href='colours.php'" class="col-lg-4">
+                        <div class="colour-items ha" data-aos="fade-up" data-aos-duration="1500">
+                            <span class="icon-sec material-icons">palette</span>
+                            <h4>Colours</h4>
+                        </div>
+                    </div>
+                    <div onclick="location.href='artkit.php'" class="col-lg-4">
+                        <div class="artkit-items ha" data-aos="fade-up" data-aos-duration="1500">
+                            <span class="icon-sec material-icons">weekend</span>
+                            <h4>Artkit</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div onclick="location.href='design.php'" class="col lg-4">
-            <div class="design-items ha">
-              <span class="icon-sec material-icons">architecture</span>
-              <h4>Designs</h4>
-            </div>
-          </div>
+            <div class="col-lg-3"></div>
         </div>
-      </div>
-      <div class="col-lg-3"></div>
+
+
+        <!--section  three-->
+        <div class="row no-gutters">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <div class="row sec-three no-gutters" data-aos="fade-up" data-aos-duration="1500">
+                    <div onclick="location.href='websites.php'" class="col-lg-8">
+                        <div class="website-items ha">
+                            <span class="icon-sec material-icons">language</span>
+                            <h4>Websites</h4>
+                        </div>
+                    </div>
+                    <div onclick="location.href='design.php'" class="col lg-4">
+                        <div class="design-items ha">
+                            <span class="icon-sec material-icons">architecture</span>
+                            <h4>Designs</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+
     </div>
 
-  </div>
-
-  <script src="vanilla.js"></script>
-  <script>
+    <script src="vanilla.js"></script>
+    <script>
     AOS.init();
-  </script>
+    </script>
 </body>
+
 </html>
